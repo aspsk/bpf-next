0010d69a5d00eadd29652509dadfcc383f22717a
Anton Protopopov <aspsk@isovalent.com>
2023-07-14 11:33:09 +0000

    bpf: make ... kfunc available to all progs
    Blah-blah-blah
    
    Signed-off-by: Anton Protopopov <aspsk@isovalent.com>

--- kernel/bpf/map_iter.c
+++ kernel/bpf/map_iter.c
@@ -198,7 +198,7 @@ __diag_push();
 __diag_ignore_all("-Wmissing-prototypes",
 		  "Global functions as their definitions will be in vmlinux BTF");
 
-__bpf_kfunc s64 bpf_map_sum_elem_count(struct bpf_map *map)
+__bpf_kfunc s64 bpf_map_sum_elem_count(const struct bpf_map *map)
 {
 	s64 *pcount;
 	s64 ret = 0;
@@ -227,6 +227,6 @@ static const struct btf_kfunc_id_set bpf_map_iter_kfunc_set = {
 
 static int init_subsystem(void)
 {
-	return register_btf_kfunc_id_set(BPF_PROG_TYPE_TRACING, &bpf_map_iter_kfunc_set);
+	return register_btf_kfunc_id_set(BPF_PROG_TYPE_UNSPEC, &bpf_map_iter_kfunc_set);
 }
 late_initcall(init_subsystem);
