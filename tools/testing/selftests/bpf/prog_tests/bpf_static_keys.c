67ee92abc6e57f1b36f5e44b78b11f99cc74742a
Anton Protopopov <a.s.protopopov@gmail.com>
2026-02-08 21:12:54 +0100

    selftests/bpf: Add tests for BPF static calls
    Add libbpf-based self-tests to test the BPF_STATIC_KEY_UPDATE syscall.
    
    TBD: check, add more tests, etc.
    
    Signed-off-by: Anton Protopopov <a.s.protopopov@gmail.com>

--- tools/testing/selftests/bpf/prog_tests/bpf_static_keys.c
+++ tools/testing/selftests/bpf/prog_tests/bpf_static_keys.c
@@ -5,6 +5,8 @@
 #include <sys/syscall.h>
 #include <bpf/bpf.h>
 
+#include "bpf_static_keys.skel.h"
+
 #define VAL_ON	7
 #define VAL_OFF	3
 
@@ -165,8 +167,178 @@ static void check_insn(void)
 	}
 }
 
+static void trigger_prog(void)
+{
+	usleep(1);
+}
+
+static void __check_one_key(struct bpf_static_keys *skel,
+			    struct bpf_map *key,
+			    int val_off,
+			    int val_on)
+{
+	int map_fd;
+	int ret;
+
+	map_fd = bpf_map__fd(key);
+	if (!ASSERT_GT(map_fd, 0, "key"))
+		return;
+
+	ret = _bpf_static_key_update(map_fd, ON);
+	if (!ASSERT_EQ(ret, 0, "_bpf_static_key_update(ON)"))
+		return;
+	skel->bss->ret_user = 0;
+	trigger_prog();
+	if (!ASSERT_EQ(skel->bss->ret_user, val_on, "skel->bss->ret_user"))
+		return;
+
+	_bpf_static_key_update(map_fd, OFF);
+	skel->bss->ret_user = 0;
+	trigger_prog();
+	if (!ASSERT_EQ(skel->bss->ret_user, val_off, "skel->bss->ret_user"))
+		return;
+}
+
+static void check_one_key(struct bpf_static_keys *skel, struct bpf_program *prog, struct bpf_map *key)
+{
+	struct bpf_link *link;
+
+	link = bpf_program__attach(prog);
+	if (!ASSERT_OK_PTR(link, "link"))
+		return;
+
+	__check_one_key(skel, key, VAL_OFF, VAL_ON);
+
+	bpf_link__destroy(link);
+}
+
+static void check_one_key_multiple(struct bpf_static_keys *skel, struct bpf_map *key)
+{
+	struct bpf_link *link;
+
+	link = bpf_program__attach(skel->progs.check_one_key_multiple);
+	if (!ASSERT_OK_PTR(link, "link"))
+		return;
+
+	__check_one_key(skel, key, VAL_OFF * 3, VAL_ON * 3);
+
+	bpf_link__destroy(link);
+}
+
+static void check_one_key_long_jump(struct bpf_static_keys *skel, struct bpf_map *key)
+{
+	struct bpf_link *link;
+
+	link = bpf_program__attach(skel->progs.check_one_key_long_jump);
+	if (!ASSERT_OK_PTR(link, "link"))
+		return;
+
+	__check_one_key(skel, key, 1000, 2000);
+
+	bpf_link__destroy(link);
+}
+
+static void __check_multiple_keys(struct bpf_static_keys *skel,
+				  struct bpf_map *key1,
+				  struct bpf_map *key2,
+				  int val_off_off,
+				  int val_off_on,
+				  int val_on_off,
+				  int val_on_on)
+{
+	int map_fd1, map_fd2;
+	int ret;
+
+	map_fd1 = bpf_map__fd(key1);
+	if (!ASSERT_GT(map_fd1, 0, "key1"))
+		return;
+
+	map_fd2 = bpf_map__fd(key2);
+	if (!ASSERT_GT(map_fd2, 0, "key2"))
+		return;
+
+	ret = _bpf_static_key_update(map_fd1, OFF);
+	if (!ASSERT_EQ(ret, 0, "_bpf_static_key_update(key1, OFF)"))
+		return;
+	ret = _bpf_static_key_update(map_fd2, OFF);
+	if (!ASSERT_EQ(ret, 0, "_bpf_static_key_update(key2, OFF)"))
+		return;
+	skel->bss->ret_user = 0;
+	trigger_prog();
+	if (!ASSERT_EQ(skel->bss->ret_user, val_off_off, "skel->bss->ret_user"))
+		return;
+
+	ret = _bpf_static_key_update(map_fd1, ON);
+	if (!ASSERT_EQ(ret, 0, "_bpf_static_key_update(key1, ON)"))
+		return;
+	ret = _bpf_static_key_update(map_fd2, OFF);
+	if (!ASSERT_EQ(ret, 0, "_bpf_static_key_update(key2, OFF)"))
+		return;
+	skel->bss->ret_user = 0;
+	trigger_prog();
+	if (!ASSERT_EQ(skel->bss->ret_user, val_off_on, "skel->bss->ret_user"))
+		return;
+
+	ret = _bpf_static_key_update(map_fd1, OFF);
+	if (!ASSERT_EQ(ret, 0, "_bpf_static_key_update(key1, OFF)"))
+		return;
+	ret = _bpf_static_key_update(map_fd2, ON);
+	if (!ASSERT_EQ(ret, 0, "_bpf_static_key_update(key2, ON)"))
+		return;
+	skel->bss->ret_user = 0;
+	trigger_prog();
+	if (!ASSERT_EQ(skel->bss->ret_user, val_on_off, "skel->bss->ret_user"))
+		return;
+
+	ret = _bpf_static_key_update(map_fd1, ON);
+	if (!ASSERT_EQ(ret, 0, "_bpf_static_key_update(key1, ON)"))
+		return;
+	ret = _bpf_static_key_update(map_fd2, ON);
+	if (!ASSERT_EQ(ret, 0, "_bpf_static_key_update(key2, ON)"))
+		return;
+	skel->bss->ret_user = 0;
+	trigger_prog();
+	if (!ASSERT_EQ(skel->bss->ret_user, val_on_on, "skel->bss->ret_user"))
+		return;
+}
+
+static void check_multiple_keys(struct bpf_static_keys *skel,
+				struct bpf_map *key1,
+				struct bpf_map *key2)
+{
+	struct bpf_link *link;
+
+	link = bpf_program__attach(skel->progs.check_multiple_keys);
+	if (!ASSERT_OK_PTR(link, "link"))
+		return;
+
+	__check_multiple_keys(skel, key1, key2, 0, 3, 30, 33);
+
+	bpf_link__destroy(link);
+}
+
+static void check_syscall(void)
+{
+	struct bpf_static_keys *skel;
+
+	skel = bpf_static_keys__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "bpf_static_keys__open_and_load"))
+		return;
+
+	check_one_key(skel, skel->progs.check_one_key_likely, skel->maps.key1);
+	check_one_key(skel, skel->progs.check_one_key_unlikely, skel->maps.key2);
+	check_one_key_multiple(skel, skel->maps.key3);
+	check_one_key_long_jump(skel, skel->maps.key4);
+	check_multiple_keys(skel, skel->maps.key5, skel->maps.key6);
+
+	bpf_static_keys__destroy(skel);
+}
+
 void test_bpf_static_keys(void)
 {
 	if (test__start_subtest("check_insn"))
 		check_insn();
+
+	if (test__start_subtest("check_syscall"))
+		check_syscall();
 }
